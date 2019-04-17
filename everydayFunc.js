80e75d4c14b8602b8d906cbea7c9b6b28517ef7f
codecatdog <1113231244@qq.com>
2019-04-17 10:08:45 +0800

    add bind ajax

--- everydayFunc.js
+++ everydayFunc.js
@@ -137,11 +137,11 @@ Element.prototype.insertAfter = function (targetNode, afterNode) {
 //优化网络请求-1： 节流
 //场景： 窗口调整(resize)  页面滚动(scroll)  抢购疯狂点击(mousedown)
 //wait: 等待时间    handler:处理函数
-function throttle (handler, wait) {
+function throttle(handler, wait) {
     var lastTime = 0;
     return function (e) {
         var nowTime = new Date().getTime();
-        if(nowTime - lastTime > wait) {
+        if (nowTime - lastTime > wait) {
             handler.apply(this, arguments);
             lastTime = nowTime;
         }
@@ -151,14 +151,85 @@ function throttle (handler, wait) {
 //优化网络请求-2：防抖
 //场景：实时搜索 拖拽
 //调用实例： oInput.oninput = debounce(handler, 1000);
-function debounce (handler, delay) {
+function debounce(handler, delay) {
     var timer = null;
     return function (e) {
         var _self = this,
             _args = arguments;
         clearTimeout(timer);
-        timer = setTimeout( function () {
+        timer = setTimeout(function () {
             handler.apply(_self, _args);
         }, delay);
     }
 }
+
+//bind的模拟实现  
+//总结：
+//1. 函数A调用bind方法时，传递的参数o, x, y, ..
+//2. 返回新的函数B
+//3. 函数B在执行的时候 具体功能还是A  只是this指向变成了o
+//4. 函数B在执行的时候 传递的参数会拼接到x, y,...后面，再执行
+//5. new B() 方式执行时构造函数依旧为A， bind(o) 不会起到作用
+
+Function.prototype.newBind = function (target) {
+    //target改变返回函数执行的this指向
+    var self = this;
+    var args = [].slice.call(arguments, 1);
+    var temp = function () {};
+    var f = function () {
+        var _arg = [].slice.call(arguments, 0);
+        //真正执行的其实是 self
+        return self.apply(this instanceof temp ? this : (target || window), args.concat(_arg));
+    }
+    temp.prototype = self.prototype;
+    f.prototype = new temp();
+    return f;
+}
+
+//getElementsByClassName 做一个兼容
+Element.prototype.getElementsByClassName = Document.prototype.getElementsByClassName
+    = document.getElementsByClassName || function (_className) {
+        var allDomArr = document.getElementsByTagName('*'); //获取所有元素
+        var lastDomArr = [];
+        for (var i = allDomArr.length - 1; i > -1; i--) {
+            var domClassName = allDomArr[i].className.split(' ');
+            for (var j = 0; j < domClassName.length; j++) {
+                if (domClassName[j].indexOf(_className) == 0 && domClassName[j].length == _className.length) {
+                    lastDomArr.push(allDomArr[i]);
+                    break;
+                }
+            }
+        }
+        return lastDomArr;
+    }
+
+    //ajax 
+    function ajax( method, url, data, isAsync, callback ) {
+        //1.create ajax object
+        var xhr = null;
+        if(window.XMLHttpRequest){
+            xhr = new XMLHttpRequest();
+        }else{
+            xhr = new ActiveXObject('Microsoft.XMLHttp');
+        }
+        //2.initialize
+        //3.send request
+        if(method == 'get' || method == 'GET'){
+            xhr.open('GET', url + '?' + data, isAsync);
+            xhr.send();
+        }else if(method == 'post'|| method == 'POST'){
+            xhr.open('POST', url, isAsync);
+            xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+            xhr.send(data);
+        }else {
+            alert('method is only GET or POST');
+        }
+        //4.response
+        xhr.onreadystatechange = function () {
+            if(xhr.readyState == 4){
+                if(xhr.status == 200){
+                    callback(xhr.responseText);
+                }
+            }
+        }
+    }
